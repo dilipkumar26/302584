892b6e6f86f8c729ded32d60aba674042bddfeb1
dilipkumar26 <67823998+dilipkumar26@users.noreply.github.com>
2021-07-12 17:39:08 +0530

    Add files via upload

---
+++ 4. Implementation/src/viewbook.c
@@ -0,0 +1,22 @@
+#include <lms_operations.h>
+void viewbook()
+{
+FILE *fp;
+int i;
+struct book bk;
+printf("\n Books in Library\n");
+fp=fopen("abc.txt","r");
+if(fp==NULL)
+{
+printf("\n File cannot open");
+return;
+}
+printf("\n**********BOOKS IN LIBRARY*****************\n");
+printf("\nACNO\t\tBOOK TITLE\tAUTHER\tAVAILABLE\n");
+while(!feof(fp))
+{
+fscanf(fp,"%d%s%s%s",&bk.accno,bk.title,bk.author,bk.isavail);
+printf("\n%d\t\t %s\t\t %s\t\t %s",bk.accno,bk.title,bk.author,bk.isavail);
+}
+fclose(fp);
+}
